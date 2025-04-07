068d63d5df5c3c978378962633acea23d96a0220
John Doe <johndoe@email.com>
2025-04-06 20:05:51 -0400

    Resolved merge conflict in AdminDashboard.java

--- src/carDealership/AdminDashboard.java
+++ src/carDealership/AdminDashboard.java
@@ -47,6 +47,7 @@ public class AdminDashboard extends JFrame implements ActionListener {
         
         //build the sidebar based on role
         sidebar = new JPanel();
+
         sidebar.setLayout(new GridLayout(0, 1, 10, 10));
         sidebar.setBackground(Color.decode("#56CCF2"));
         sidebar.setPreferredSize(new Dimension(200, getHeight()));
